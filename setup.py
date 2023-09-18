d85c6ca81485da44c500d89f873d9b509c3f3ef3
Swayam <74960567+practice404@users.noreply.github.com>
2023-09-18 16:23:51 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='recursion_trace',
-    version='0.1.5',
+    version='0.2.0',
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type="text/markdown",
