2602f0f907983497d239319a77abb5c97bd84f7f
Swayam <74960567+practice404@users.noreply.github.com>
2023-09-18 09:56:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='recursion_trace',
     version='0.1.1',
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[
         'graphviz',
     ],
