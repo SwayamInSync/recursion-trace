49b5f9202f52a8b804887231637cc21093876711
Swayam <74960567+practice404@users.noreply.github.com>
2023-09-18 09:42:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='recursion_trace',
-    version='1.0.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'graphviz',
