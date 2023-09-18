9e40b9bb1640306fa4029a0d806a9430b9004b38
Swayam <74960567+practice404@users.noreply.github.com>
2023-09-18 16:23:36 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,7 @@ setup(
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=[
-        'graphviz',
-    ],
+    install_requires=['graphviz', 'matplotlib', 'pydot', 'networkx'],
     author='Swayam Singh',
     author_email='hawkempire007@gmail.com',
     description='A package to trace recursive function calls and generate a recursion tree'
